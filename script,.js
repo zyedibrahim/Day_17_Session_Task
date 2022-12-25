0c780a547333a72c8c5f93f20a751d06f6d84a33
zyedibrahim <zyedibrahim77@gmail.com>
2022-12-25 15:16:02 +0530

    first commit

---
+++ script,.js
@@ -0,0 +1,321 @@
+
+// var display = document.querySelector('.container')
+
+//  var create_span = document.querySelector('.span')
+
+
+
+//  create_span.textContent = `${one}`
+// display.append(create_span )
+
+
+
+
+
+
+
+ var zero = function (one1){
+    one1(one)
+    
+      setTimeout(()  => {
+
+        var display = document.querySelector('.container')
+
+        var create_span = document.querySelector('.span')
+        var create_img =  document.createElement('img')
+        
+        var create_spanlines = document.createElement('p')
+        create_spanlines.textContent= "HAPPY INDEPENDENCE DAY "
+        create_img.setAttribute('src','https://upload.wikimedia.org/wikipedia/en/4/41/Flag_of_India.svg' )
+       create_img.setAttribute('alt','flags' )
+       create_img.style.width= "300px";
+       
+       
+        
+       display.append(create_span )
+       create_span.textContent = ''
+       create_span.append(create_img)
+
+       create_span.append(create_spanlines)
+    
+        console.log("0 t")
+        
+        
+
+}, 11000 )
+
+
+}
+
+
+
+
+ var one = function (t2o){
+    t2o(two)
+    
+      setTimeout(()  => {
+
+        var display = document.querySelector('.container')
+
+        var create_span = document.querySelector('.span')
+       
+       
+       
+        create_span.textContent = "1"
+       display.append(create_span )
+    
+        console.log("1 t")
+        
+        
+
+}, 10000 )
+
+
+}
+
+
+
+ 
+ var two = function (th3ee){
+    th3ee(three)
+    
+    setTimeout(()  => {
+
+
+        var display = document.querySelector('.container')
+
+        var create_span = document.querySelector('.span')
+       
+       
+       
+        create_span.textContent = "2"
+       display.append(create_span )
+
+        
+
+console.log("2t")
+
+}, 9000 )
+
+
+
+}
+
+
+
+ var three= function (four4){
+    four4(four)
+    
+    setTimeout(()  => {
+
+
+        var display = document.querySelector('.container')
+
+        var create_span = document.querySelector('.span')
+       
+       
+       
+        create_span.textContent = "3"
+       display.append(create_span )
+    
+        
+
+
+
+console.log("3t")
+
+}, 8000 )
+
+
+
+}
+
+
+
+
+
+ var four = function (five5){
+    five5(five)
+    
+    setTimeout(()  => {
+
+
+        var display = document.querySelector('.container')
+
+        var create_span = document.querySelector('.span')
+       
+       
+       
+        create_span.textContent = "4"
+       display.append(create_span )
+    
+        
+
+console.log("4t")
+
+}, 7000 )
+
+
+
+}
+
+ 
+ var five= function (six6){
+    six6(six)
+
+
+    setTimeout(()  => {
+
+
+
+
+        var display = document.querySelector('.container')
+
+        var create_span = document.querySelector('.span')
+       
+       
+       
+        create_span.textContent = "5"
+       display.append(create_span )
+       
+        
+
+console.log("5")
+
+}, 6000 )
+
+    
+}
+
+
+
+
+  
+ var six = function (seven7){
+    seven7(seven)
+    
+    setTimeout(()  => {
+
+
+        var display = document.querySelector('.container')
+
+        var create_span = document.querySelector('.span')
+       
+       
+       
+        create_span.textContent = "6"
+       display.append(create_span )
+       
+        
+
+console.log("6")
+
+}, 5000 )
+
+
+}
+
+
+
+
+
+ var seven = function (eight8){
+    eight8(eight)
+
+
+    setTimeout(()  => {
+
+        var display = document.querySelector('.container')
+
+        var create_span = document.querySelector('.span')
+       
+       
+       
+        create_span.textContent = "7"
+       display.append(create_span )
+       
+        
+
+console.log("7")
+
+}, 4000 )
+
+
+
+}
+
+
+
+ var eight = function (nine9){
+    nine9(nine)
+    
+    setTimeout(()  => {
+
+
+        var display = document.querySelector('.container')
+
+        var create_span = document.querySelector('.span')
+       
+       
+       
+        create_span.textContent = "8"
+       display.append(create_span )
+       
+        
+
+console.log("8")
+
+}, 3000 )
+
+
+}
+
+
+
+
+ var nine = function (ten10){
+    
+    ten10(ten)
+    setTimeout(()  => {
+        
+
+        var display = document.querySelector('.container')
+
+        var create_span = document.querySelector('.span')
+       
+       
+       
+        var inv = create_span.textContent = "9"
+       display.append(create_span )
+    
+        
+
+console.log("9")
+
+}, 2000 )
+    
+
+}
+ var ten = function (){
+setTimeout(()  => {
+
+
+    var display = document.querySelector('.container')
+
+    var create_span = document.querySelector('.span')
+   
+   
+   
+    create_span.textContent = "10"
+   display.append(create_span )
+    
+    console.log("10")
+
+}, 1000 )
+
+
+
+ }
+
+
+zero(zero)
